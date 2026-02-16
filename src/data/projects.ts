24129946fc9bbf5bb45c338d3c488833d276ae8c
LisaWerner856 <lwerner@st.noorderpoort.nl>
2026-02-16 11:18:34 +0100

    Uploading images, updated links in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -22,48 +22,40 @@ export const projects: Project[] = [
       'I recreated the classic simon says game (electronic version) with an arduino uno.',
     tags: ['arduino', 'c/c++'],
     status: 'completed',
-    image: 'https://raw.githubusercontent.com/LisaWerner856/lisawerner856.github.io/main/image/arduino1.jpg',
-    githubUrl: 'https://github.com/LisaWerner856',
+    image: './image/arduino1.jpg',
+    githubUrl: 'https://github.com/LisaWerner856/KD-Microcontroller',
   },
   {
     slug: 'portfolio-website',
     title: 'Portfolio website',
     description: 'The Repository for my website.',
     tags: ['html', 'css/tailwind'],
-    status: 'completed',
-    image: 'https://raw.githubusercontent.com/LisaWerner856/lisawerner856.github.io/main/image/portfoliowebsite-screenshot.png',
+    status: 'in-progress',
+    image: './image/portfoliowebsite-screenshot.png',
     githubUrl: 'https://github.com/LisaWerner856/lisawerner856.github.io',
     blogContent: {
-      heading: 'How I Built My Website from Scratch',
-      intro: 'Building my website from scratch was a rewarding process. Here\'s a quick breakdown:',
+      heading: 'Builind My Website',
+      intro: 'The process of building the frontend of my website:',
       sections: [
         {
           title: 'Planning and Design',
-          text: 'I began by sketching a wireframe and planning the site\'s structure for an intuitive user experience.',
-        },
+          text: 'I began by looking for inspiration. Then, I started designing a wireframe and planning the sites structure in Figma.',
+        }, 
         {
-          title: 'Writing HTML',
-          text: 'I wrote clean, semantic HTML to organize the content and ensure accessibility and SEO.',
-        },
+          title: 'HTML & CSS (+Tailwind)',
+          text: 'I began by building out the wireframe from figm in plain HTML and tailwind. Once that was completed, I moved on to making the mobile version. ',
+        }, 
         {
-          title: 'Styling with Tailwind CSS',
-          text: 'I used Tailwind CSS, a utility-first framework, to style the site. It allowed me to quickly customize elements like typography, colors, and spacing without writing extensive custom CSS.',
-        },
+          title: 'Experimenting with React + Vite',
+          text: 'After having the HTML and CSS of my site built, I set up a basic React + Vite project and started to recreate my website.',
+        }, 
         {
-          title: 'Interactivity',
-          text: 'I added simple JavaScript for features like modals and smooth scrolling, keeping the site lightweight and user-friendly.',
-        },
-        {
-          title: 'Optimization and Testing',
-          text: 'I optimized the site for speed and tested it across browsers and devices for compatibility.',
-        },
-        {
-          title: 'Launching',
-          text: 'Once everything was polished, I deployed the site using GitHub Pages, making it live for everyone to see.',
-        },
+          title: 'Hosting on GithubPages',
+          text: 'Lastly, all that was left was to build and deploy the project. (And fix some errors)',
+        }
       ],
       conclusion:
-        'Building this site from scratch allowed me to create a unique, responsive experience that reflects my vision. I\'m proud of the final result!',
+        'I re-built this site with react + vite to gain some experience with those technologies. I also used V0 and ChatGPT during this project.',
     },
   },
   {
@@ -72,9 +64,9 @@ export const projects: Project[] = [
     description:
       'A console application textbased rpg, inspired by the classic game Zork. The setting is the Deep Space Nine space station. You have just graduated from Starfleet academy, ready to start your first assignment.',
     tags: ['c#'],
-    status: 'completed',
+    status: 'in-progress',
     image: 'https://raw.githubusercontent.com/LisaWerner856/lisawerner856.github.io/main/image/TextAdventure-DS9-screenshot.png',
-    githubUrl: 'https://github.com/LisaWerner856',
+    githubUrl: 'https://github.com/LisaWerner856/TextbasedAdventure',
   },
 ]
 
